57d44829c37bf0e0af9d69b978e7c7b27a888460
yt <asnedge.yt@gmail.com>
2022-01-25 00:40:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Usage
+
+```
+docker-compose up -d
+docker-compose exec front bash
+npm start
+```
+
+# Description
+
+- Docker で Node.js(tsx) の環境を作るためのベースリポジトリ
---
+++ front/app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles/App.css'
+
+const App: React.FC = () => (
+  <div className='App'>
+    <h1>Hello React with TS project</h1>
+  </div>
+)
+
+export default App
